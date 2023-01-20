3db528bbf8b7eaad15398acfb72dfae7c89abe44
Jose De La Cruz <jose.delcruz0490@icloud.com>
2023-01-20 06:57:27 -0500

    Completed exercise 01 of lesson 04

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -6,15 +6,15 @@ import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
-test('submitting the form calls onSubmit with username and password', () => {
+test('submitting the form calls onSubmit with username and password', async () => {
   // 🐨 create a variable called "submittedData" and a handleSubmit function that
   // accepts the data and assigns submittedData to the data that was submitted
   // 💰 if you need a hand, here's what the handleSubmit function should do:
   // const handleSubmit = data => (submittedData = data)
-  //
+
   // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
+  // 🐨 get the username and password fields via
+  // `getByLabelText` - This will search for the label that matches the given TextMatch, then find the element associated with that label and will throw a descriptive error if no elements match
   // 🐨 use `await userEvent.type...` to change the username and password fields to
   //    whatever you want
   //
@@ -22,6 +22,21 @@ test('submitting the form calls onSubmit with username and password', () => {
   //
   // assert that submittedData is correct
   // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+  let submittedData
+  const handleSubmit = data => (submittedData = data)
+
+  render(<Login onSubmit={handleSubmit} />)
+  const username = screen.getByLabelText(/username/i)
+  const password = screen.getByLabelText(/password/i)
+  const submitBtn = screen.getByRole('button', {type: 'Submit'})
+
+  await userEvent.type(username, 'jose-a-dlc05')
+  await userEvent.type(password, 'password')
+  await userEvent.click(submitBtn)
+  expect(submittedData).toEqual({
+    username: 'jose-a-dlc05',
+    password: 'password',
+  })
 })
 
 /*
