ab07f6581786663633be71d3ed7ccfb64a56b221
Jose De La Cruz <jose.delcruz0490@icloud.com>
2023-01-30 19:41:18 -0500

    Completed lesson 08 exercise 01

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -11,12 +11,31 @@ import useCounter from '../../components/use-counter'
 // capabilities of this hook
 // 💰 here's how to use the hook:
 // const {count, increment, decrement} = useCounter()
+const CounterApp = () => {
+  const {count, increment, decrement} = useCounter()
+  return (
+    <div>
+      <div>Current count: {count}</div>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
+}
 
-test('exposes the count and increment/decrement functions', () => {
+test('exposes the count and increment/decrement functions', async () => {
   // 🐨 render the component
+  render(<CounterApp />)
   // 🐨 get the elements you need using screen
+  const countDisplay = screen.getByText(/current/i)
+  const incrementBtn = screen.getByText(/increment/i)
+  const decrementBtn = screen.getByText(/decrement/i)
   // 🐨 assert on the initial state of the hook
+  expect(countDisplay.innerHTML).toBe('Current count: 0')
   // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  await userEvent.click(incrementBtn)
+  expect(countDisplay.innerHTML).toBe('Current count: 1')
 })
+
+// Some other tests you can write are for the props
 
 /* eslint no-unused-vars:0 */
