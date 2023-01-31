7b95fa9a2f88feccc0652d08ae78a07e4b205cb4
Jose De La Cruz <jose.delcruz0490@icloud.com>
2023-01-30 19:54:01 -0500

    Completed lesson 8 extra credit 1

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -5,35 +5,21 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
+import {act} from 'react-test-renderer'
 
-// 🐨 create a simple function component that uses the useCounter hook
-// and then exposes some UI that our test can interact with to test the
-// capabilities of this hook
-// 💰 here's how to use the hook:
-// const {count, increment, decrement} = useCounter()
-const CounterApp = () => {
-  const {count, increment, decrement} = useCounter()
-  return (
-    <div>
-      <div>Current count: {count}</div>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={increment}>Increment</button>
-    </div>
-  )
-}
+test('exposes the count and increment/decrement functions', () => {
+  let result
+  function TestComponent(props) {
+    result = useCounter(props)
+    return null
+  }
 
-test('exposes the count and increment/decrement functions', async () => {
-  // 🐨 render the component
-  render(<CounterApp />)
-  // 🐨 get the elements you need using screen
-  const countDisplay = screen.getByText(/current/i)
-  const incrementBtn = screen.getByText(/increment/i)
-  const decrementBtn = screen.getByText(/decrement/i)
-  // 🐨 assert on the initial state of the hook
-  expect(countDisplay.innerHTML).toBe('Current count: 0')
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
-  await userEvent.click(incrementBtn)
-  expect(countDisplay.innerHTML).toBe('Current count: 1')
+  render(<TestComponent />)
+  expect(result.count).toBe(0)
+  act(() => result.increment())
+  expect(result.count).toBe(1)
+  act(() => result.decrement())
+  expect(result.count).toBe(0)
 })
 
 // Some other tests you can write are for the props
